0989f66c487bde049923452682eb881ef71e7c67
Enzo <enzoabatrov@hotmail.com>
2019-06-30 20:28:25 -0300

    Project Refactor and added system of saving data to files

--- app.js
+++ app.js
@@ -2,32 +2,25 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
-//const hdbs = require('express-handlebars');
+
+const errorController = require('./controllers/error');
 
 const app = express();
 
-//app.engine('hbs', hdbs({layoutsDir: 'views/layouts/', defaultLayout: 'main-layout', extname: 'hbs'}));
-//app.set('view engine', 'hbs');
-
-//app.set('view engine', 'pug');
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-const adminData = require('./routes/admin');
+const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin', adminData.routes);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-app.use((req, res, next) => {
-    res.status(404).render('404', {pageTitle: '404 - Page Not Found'});
-});
+app.use(errorController.get404);
 
-const port = 3000
-
-app.listen(port, () => {
-    console.log(`Server running on port ${port}`)
+app.listen(3000, () => {
+    console.log('Server running on port 3000');
 });
